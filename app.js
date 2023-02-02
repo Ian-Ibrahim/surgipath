b53e97179783f7affcd1f6d1c8fd99e8bd466086
Ian-Ibrahim <ian.okova@strathmore.edu>
2023-02-02 12:07:34 +0300

    pre-revision commit

---
+++ app.js
@@ -0,0 +1,19 @@
+
+function openCategoryPage(category) {
+    window.location.href = `category.html?category=${category}`;
+}
+const dateToday = new Date();
+document.getElementById("get-current-year").innerHTML=dateToday.getFullYear()
+$(document).ready(function () {
+    $("#partners-slider").owlCarousel({
+        items: 3,
+        itemsDesktop: [1000, 3],
+        itemsDesktopSmall: [980, 2],
+        itemsTablet: [768, 2],
+        itemsMobile: [650, 1],
+        pagination: false,
+        navigation: false,
+        slideSpeed: 100,
+        autoPlay: true
+    });
+});
